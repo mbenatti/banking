0068dedc31558b3917a1429f928302ce5e841a32
Marcos Benatti Lauer <benatti77@gmail.com>
2019-03-20 19:17:36 -0300

    setup: added deps: credo, ex_docs, excoverrals and dialyxir

--- mix.exs
+++ mix.exs
@@ -15,6 +15,11 @@ defmodule Banking.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+     {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+     {:dialyxir, "~> 1.0.0-rc.4", only: [:dev], runtime: false},
+     {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
+     {:excoveralls, "~> 0.10", only: [:dev, :test]},
+    ]
   end
 end
