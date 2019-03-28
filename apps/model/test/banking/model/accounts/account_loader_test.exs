3a6db4840b4f34e3111dd021dfff6ea41ca198dd
Marcos Benatti Lauer <benatti77@gmail.com>
2019-03-28 20:03:09 -0300

    test: account read operations test

---
+++ apps/model/test/banking/model/accounts/account_loader_test.exs
@@ -0,0 +1,46 @@
+defmodule Banking.Model.Accounts.AccountLoaderTest do
+  @moduledoc"""
+  Tests for read operations on account
+  """
+  use Banking.Model.DataCase
+
+  alias Banking.Model.Accounts.{AccountLoader, AccountSchema}
+
+  import Banking.Model.Factory
+
+  describe("get account") do
+    test "by id" do
+      %AccountSchema{id: id_inserted, name: name_inserted} = insert(:account)
+
+      %AccountSchema{id: id, name: name} = AccountLoader.get(id_inserted)
+
+      assert id_inserted == id
+      assert name_inserted == name
+    end
+
+    test "by email" do
+      %AccountSchema{id: id_inserted, name: name_inserted, email: email} = insert(:account)
+
+      %AccountSchema{id: id, name: name} = AccountLoader.get_by_email(email)
+
+      assert id_inserted == id
+      assert name_inserted == name
+    end
+  end
+
+  describe("get_all/2") do
+    test "retrieve accounts" do
+      insert_list(2, :account)
+      
+      accounts = AccountLoader.get_all()
+
+      assert Enum.count(accounts) == 2
+    end
+
+    test "without accounts" do
+      accounts = AccountLoader.get_all()
+
+      assert accounts == []
+    end
+  end
+end
