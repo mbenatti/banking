4112ddc3a6e0afa13533a35080b6b27a9d4cecd1
Marcos Benatti Lauer <benatti77@gmail.com>
2019-04-03 22:32:52 -0300

    feature: implement logic for auth on accounts app

---
+++ apps/accounts/lib/accounts/auth.ex
@@ -0,0 +1,22 @@
+defmodule Banking.Accounts.Auth do
+  @moduledoc """
+  Responsible for authentications operation`s
+  """
+
+  alias Banking.Model.Accounts.{AccountLoader, AccountSchema}
+
+  @doc """
+  Authenticate the user by email and password
+  """
+  @spec authenticate(String.t(), String.t()) ::
+          {:ok, AccountSchema.t()} | {:error, :not_found} | {:error, :unauthorized}
+  def authenticate(email, password) do
+    with client when not is_nil(client) <- AccountLoader.get_by_email(email),
+         true <- Argon2.verify_pass(password, client.password) do
+      {:ok, client}
+    else
+      nil -> {:error, :not_found}
+      false -> {:error, :unauthorized}
+    end
+  end
+end
