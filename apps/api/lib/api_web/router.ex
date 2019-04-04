345c01db28244f85f9db1c0c117b57c06a1bad3a
Marcos Benatti Lauer <benatti77@gmail.com>
2019-04-03 22:59:23 -0300

    feature: router with auth pipelines, register and auth

--- apps/api/lib/api_web/router.ex
+++ apps/api/lib/api_web/router.ex
@@ -5,7 +5,27 @@ defmodule Banking.APIWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :auth do
+    plug(Banking.APIWeb.Pipeline.Auth)
+  end
+
+  pipeline :ensure_auth do
+    plug(Guardian.Plug.EnsureAuthenticated)
+    plug(Banking.APIWeb.Plugs.CurrentClient)
+  end
+
   scope "/api", Banking.APIWeb do
     pipe_through :api
+
+    post "/registration", AccountController, :create
+
+    scope "/auth" do
+      post "/", AuthController, :create
+    end
+
+    scope "/priv", AccountController do
+      pipe_through [:auth, :ensure_auth]
+
+    end
   end
 end
