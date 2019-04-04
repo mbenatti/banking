1f18892c37b3e618f1b4294485d5974cb2110278
Marcos Benatti Lauer <benatti77@gmail.com>
2019-04-03 22:57:26 -0300

    feature: account controller with registration on API

---
+++ apps/api/lib/api_web/controllers/accounts/account_controller.ex
@@ -0,0 +1,27 @@
+defmodule Banking.APIWeb.AccountController do
+  @moduledoc """
+  Routes implementation for Account
+  """
+
+  use Banking.APIWeb, :controller
+
+  alias Banking.Accounts.Register
+  alias Plug.Conn
+
+  action_fallback(Banking.APIWeb.FallbackController)
+
+  @doc """
+  Create an user
+
+  ## Parameters
+
+    -- conn: The connectio
+    -- params: The params provided by user containing the name, username(email) and password
+  """
+  @spec create(Conn.t(), Map.t()) :: Plug.Conn.t()
+  def create(conn, %{} = params) do
+    with {:ok, account} <- Register.create(params) do
+      render(conn, "create.json", account: account)
+    end
+  end
+end
