a5a99ffdc8e950215cf5a8654742e6cef7a1aec8
Marcos Benatti Lauer <benatti77@gmail.com>
2019-04-07 01:26:02 -0300

    feature: Queries for Balance event context

---
+++ apps/model/lib/banking/model/balances/event_queries.ex
@@ -0,0 +1,32 @@
+defmodule Banking.Model.Balances.EventQueries do
+  @moduledoc """
+  The queries for the Balance Event context.
+  """
+
+  alias Banking.Model.Balances.EventSchema
+
+  import Ecto.Query
+
+  @doc """
+  Return a query for a select all
+  """
+  @spec all() :: EventSchema.t()
+  def all, do: EventSchema
+
+  @doc """
+  Return the query to get last balance event by account id
+
+  ## Examples
+
+      iex> EventQueries.get_last_by_account_id(2)
+      #Ecto.Query<from e0 in Banking.Model.Balances.EventSchema,
+      where: e0.account_id == ^2, order_by: [desc: e0.inserted_at], limit: 1>
+
+  """
+  @spec get_last_by_account_id(Integer.t()) :: Query.t()
+  def get_last_by_account_id(account_id) do
+    all()
+    |> where([a], a.account_id == ^account_id)
+    |> last(:inserted_at)
+  end
+end
