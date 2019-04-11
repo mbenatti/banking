a27abb423b8871d1544e36f1bd047cac59248043
Marcos Benatti Lauer <benatti77@gmail.com>
2019-04-10 23:24:14 -0300

    feature: router.ex with backoffice and transactions routes

--- apps/api/lib/api_web/router.ex
+++ apps/api/lib/api_web/router.ex
@@ -14,6 +14,10 @@ defmodule Banking.APIWeb.Router do
     plug(Banking.APIWeb.Plugs.CurrentClient)
   end
 
+  pipeline :auth_backoffice do
+    plug BasicAuth, use_config: {:api, :backoffice_auth}
+  end
+
   scope "/api", Banking.APIWeb do
     pipe_through :api
 
@@ -23,9 +27,23 @@ defmodule Banking.APIWeb.Router do
       post "/", AuthController, :create
     end
 
-    scope "/priv", AccountController do
+    scope "/priv" do
       pipe_through [:auth, :ensure_auth]
 
+      post "/deposit", TransactionController, :deposit
+      post "/withdrawal", TransactionController, :withdrawal
+      post "/transfer", TransactionController, :transfer
+      get "/balance", TransactionController, :balance
+      get "/statement", TransactionController, :statement
+    end
+
+    scope "/admin" do
+      pipe_through [:auth_backoffice]
+
+      get "/report/daily", BackofficeController, :daily_report
+      get "/report/monthly", BackofficeController, :monthly_report
+      get "/report/yearly", BackofficeController, :yearly_report
+      get "/report/total", BackofficeController, :total_report
     end
   end
 end
